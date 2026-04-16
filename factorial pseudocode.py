971caebd7b518d739c69fd4c2f42d50d3796bc71
Ma-thews <chandamathews031@gmail.com>
2026-04-16 22:46:02 +0200

    Delete factorial pseudocode.py
    Delete

--- factorial pseudocode.py
+++
@@ -1,9 +0,0 @@
-FUNCTION factorial(n):
-    IF n < 0 THEN
-        RETURN "Error"
-    ELSE IF n == 0 OR n == 1 THEN
-        RETURN 1
-    ELSE
-        RETURN n * factorial(n - 1)
-    END IF
-END FUNCTION
